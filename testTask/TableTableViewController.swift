5b176ef795410cee082c8bb9f601d85db93664e5
Maksym <maks.mihalchuk.2000@gmail.com>
2020-03-09 01:20:42 +0200

    Category display

--- testTask/TableTableViewController.swift
+++ testTask/TableTableViewController.swift
@@ -15,9 +15,19 @@ class TableTableViewController: UITableViewController {
     let arrayCategories = ["arts", "automobiles", "books", "business", "climate"]
     override func viewDidLoad() {
         super.viewDidLoad()
-
-     
     }
-
-    
+// MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayCategories.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let arrayCategoriesView = arrayCategories[indexPath.row]
+        cell.textLabel?.text = arrayCategoriesView
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
 }
